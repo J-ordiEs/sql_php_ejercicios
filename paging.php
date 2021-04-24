2d2438d0b190e99a2bc573b81e61b59422b0f685
J-ordiEs <70259721+J-ordiEs@users.noreply.github.com>
2021-04-24 08:42:10 +0200

    aplicando paginacion

--- paging.php
+++ paging.php
@@ -15,9 +15,19 @@ foreach( $search_arr as $search_term )
   $n++;
 }
 
+$statement_count = $pdo->prepare($sql);
+$statement_count->execute($arr_sql_terms);
+$results_without_paging = COUNT( $statement_count->fetchAll() ) ;
+// echo $results_without_paging;
 
+$total_rows_to_show = 10;
 
-$sql .= " LIMIT 0,50";
+$total_pages_to_show =  ceil($results_without_paging/$total_rows_to_show);
+
+$current_page = isset( $_GET['page']) ? $_GET['page'] : 0 ;
+$sql_page_param = $current_page*$total_rows_to_show;
+
+$sql .= " LIMIT {$sql_page_param},{$total_rows_to_show}";
 
 $statement = $pdo->prepare($sql);
 $statement->execute($arr_sql_terms);
@@ -25,80 +35,83 @@ $results = $statement->fetchAll();
 ?>
 <!doctype html>
 <html class="no-js" lang="en">
+
 <head>
-<meta charset="utf-8"/>
-<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-<title>PHP & SQL</title>
-<link rel="stylesheet" href="http://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>PHP & SQL</title>
+  <link rel="stylesheet" href="http://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
 </head>
+
 <body>
- 
-<div class="top-bar">
-<div class="top-bar-left">
-<ul class="menu">
-<li class="menu-text">Curso PHP & SQL</li>
-</ul>
-</div>
-</div>
- 
-<div class="row column text-center">
-<h2>Paginación</h2>
-<hr>
-</div>
-<div class="row column">
-<div class="callout primary">
-<h3>Noticias</h3>
-<form method="get">
-  <div class="row">
-    <div class="medium-6 columns">
-      <label>Ingrese el título
-        <input type="text" name="title" placeholder="ej. javascript" value="<?php echo $seach_terms; ?>">
-        <input class="button" type="submit" value="BUSCAR" />
-      </label>
+
+  <div class="top-bar">
+    <div class="top-bar-left">
+      <ul class="menu">
+        <li class="menu-text">Curso PHP & SQL</li>
+      </ul>
     </div>
   </div>
-</form>
-</div>
-<table width="100%">
-  <thead>
-    <tr>
-      <th>ID</th>
-      <th>Título</th>
-      <th>Contenido</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php
+
+  <div class="row column text-center">
+    <h2>Paginación</h2>
+    <hr>
+  </div>
+  <div class="row column">
+    <div class="callout primary">
+      <h3>Noticias</h3>
+      <form method="get">
+        <div class="row">
+          <div class="medium-6 columns">
+            <label>Ingrese el título
+              <input type="text" name="title" placeholder="ej. javascript" value="<?php echo $seach_terms; ?>">
+              <input class="button" type="submit" value="BUSCAR" />
+            </label>
+          </div>
+        </div>
+      </form>
+    </div>
+    <table width="100%">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Título</th>
+          <th>Contenido</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
     foreach($results as $rs)
     {
   ?>
-    <tr>
-      <td width="300"><?php echo $rs['id']; ?></td>
-      <td width="300"><?php echo $rs['title']; ?></td>
-      <td><?php echo $rs['content']; ?></td>
-    </tr>
-    <?php
+        <tr>
+          <td width="300"><?php echo $rs['id']; ?></td>
+          <td width="300"><?php echo $rs['title']; ?></td>
+          <td><?php echo $rs['content']; ?></td>
+        </tr>
+        <?php
   }
     ?>
-  </tbody>
-</table>
-</div>
-<hr>
+      </tbody>
+    </table>
+  </div>
+  <hr>
 
-</div>
-<div class="large-3 large-offset-2 columns">
-<ul class="pagination text-center" role="navigation" aria-label="Pagination">
-  <!--<li class="current">1</li>-->
-  <li><a href="paging.php?page=1" aria-label="Page 1">1</a></li>
-  <li><a href="paging.php?page=2" aria-label="Page 2">2</a></li>
-  <li><a href="paging.php?page=3" aria-label="Page 3">3</a></li>
-</ul>
-</div>
-</div>
-<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-<script src="http://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
-<script>
-      $(document).foundation();
-    </script>
+  </div>
+  <div class="large-3 large-offset-2 columns">
+    <ul class="pagination text-center" role="navigation" aria-label="Pagination">
+      <!--<li class="current">1</li>-->
+      <?php for($i = 1; $i <= $total_pages_to_show; $i++){?>
+      <li><a href="paging.php?page=<?php echo $i-1; ?>" aria-label="Page <?php echo $i; ?>"><?php echo $i; ?></a></li>
+      <?php }?>
+    </ul>
+  </div>
+  </div>
+  <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+  <script src="http://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
+  <script>
+  $(document).foundation();
+  </script>
 </body>
+
 </html>
